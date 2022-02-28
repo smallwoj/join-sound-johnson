bfa4e39fc7c0b4007e0558a9db580bcc5dc64ec5
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-02-28 14:14:58 -0600

    start building the bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use poise::serenity_prelude as serenity;
+
+type Data = ();
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+const TOASTLORD_ID: serenity::UserId = serenity::UserId(90237200909729792);
+
+// Pong up
+#[poise::command(prefix_command, slash_command, track_edits)]
+pub async fn ping(
+    ctx: Context<'_>,
+) -> Result<(), Error>
+{
+    ctx.say("Pong!").await?;
+
+    Ok(())
+}
+
+/// Register application commands in this guild or globally
+///
+/// Run with no arguments to register in guild, run with argument "global" to register globally.
+#[poise::command(prefix_command, hide_in_help)]
+async fn register(ctx: Context<'_>, #[flag] global: bool) -> Result<(), Error>
+{
+    if ctx.author().id == TOASTLORD_ID
+    {
+        poise::builtins::register_application_commands(ctx, global).await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main()
+{
+    dotenv::dotenv().ok();
+
+    let token = std::env::var("DISCORD_BOT_TOKEN").expect("Expected a token in the environment");
+    let application_id = std::env::var("DISCORD_APPLICATION_ID").expect("Expected an application id in the environment");
+
+    poise::Framework::build()
+        .token(token)
+        .application_id(application_id)
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(()) }))
+        .options(poise::FrameworkOptions {
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("j!".into()),
+                ..Default::default()
+            },
+            commands: vec![
+                ping(),
+                register(),
+            ],
+            ..Default::default()
+        })
+        .run().await.unwrap();
 }
